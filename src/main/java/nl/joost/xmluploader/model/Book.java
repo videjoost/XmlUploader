bd34573084799e5556aac0b60b353f748e4b1686
joost <joost.jonge@koop.overheid.nl>
2024-09-21 10:57:49 +0200

    Adding a book class

--- src/main/java/nl/joost/xmluploader/model/Book.java
+++ src/main/java/nl/joost/xmluploader/model/Book.java
@@ -1,5 +1,39 @@
 package nl.joost.xmluploader.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "book")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 
+  @Id
+  private int id;
+
+  @Column(name = "author", nullable = false)
+  private String author;
+
+  @Column(name = "title", nullable = false)
+  private String title;
+
+  @Column(name = "genre", nullable = false)
+private String genre;
+
+  @Column(name = "price", nullable = false)
+  private double price;
+
+  @Column(name = "publish_date", nullable = false)
+  private String publishDate;
+
+  @Column(name = "description", nullable = false)
+  private String description;
 }
