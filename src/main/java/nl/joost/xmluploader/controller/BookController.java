0e424034e71287c3b5d408c670a97c89a0d3926b
joost <joost.jonge@koop.overheid.nl>
2024-09-22 09:05:28 +0200

    Added Bookcontroller to list books

--- src/main/java/nl/joost/xmluploader/controller/BookController.java
+++ src/main/java/nl/joost/xmluploader/controller/BookController.java
@@ -1,6 +1,28 @@
 package nl.joost.xmluploader.controller;
 
+import org.springframework.ui.Model;
+import java.util.List;
+import nl.joost.xmluploader.model.Book;
+import nl.joost.xmluploader.repo.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class BookController {
+
+  private final BookRepo bookrepo;
+
+  @Autowired
+  public BookController(BookRepo bookrepo) {
+    this.bookrepo = bookrepo;
+  }
+
+  @GetMapping("/books")
+  public String listBooks(Model model) {
+    List<Book> books = bookrepo.findAll();
+    model.addAttribute("books", books);
+    return "books";
+  }
 
 }
